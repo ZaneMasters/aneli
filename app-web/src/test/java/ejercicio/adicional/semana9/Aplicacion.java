dd2a438b658dbd62fe5783ca91d93faeccd91d1a
Angel Eduardo Rodriguez Arguello <angel.pro98@gmail.com>
2022-05-07 23:24:58 -0500

    complementos

--- app-web/src/test/java/ejercicio/adicional/semana9/Aplicacion.java
+++ app-web/src/test/java/ejercicio/adicional/semana9/Aplicacion.java
@@ -11,73 +11,107 @@ public class Aplicacion {
 		Menu menu1= new Menu();		
 		Alumno lu= new Alumno();
 		Materia ma=new Materia();
+		DatosDBMemoria datas= new DatosDBMemoria();
 		boolean opcioncorrecta=true;
 		int a=0;
-		while(opcioncorrecta) {
-			menu1.menuprincipal();
-			
-			System.out.println("Seleccione una opcion: ");
-			a = teclado.nextInt();
-			
-	        if (a<1 || a> 4)
-	            System.out.println("Opción incorrecta, ingrese nuevamente... ");
-	        else {
-	        	System.out.println("iGracias por usar este sistema!");
-	        	opcioncorrecta=false;
-	        	break;
-	        }
-		}
-		System.out.println("===============================================================");
-		System.out.println("Inserte el numero del estudiante para inscribir materia mostrado acontinuacion:");
-		int i=0;
-		for (String uwu:lu.Nombre) {
-			System.out.println(i+". "+uwu + " "+ lu.Apellido[i]);
-			i=i+1;
-		}
-		int b = teclado.nextInt();
+		boolean menues=true;
 		
-		switch (a) {
-		case 1:
-			System.out.println("Inserte el ID de la materia que desea inscribir para el estudiante: " + lu.Nombre[b]+" "+lu.Apellido[b]);
-			i=0;
-			for (String uwu:ma.materia) {
-				System.out.println("ID:"+ma.ID[i]+" "+uwu);
-				i=i+1;
-			}
-			
-			opcioncorrecta=true;
+		do {
 			while(opcioncorrecta) {
-				int c = teclado.nextInt();
-				if(c==123 || c==265 || c==348 || c==489) {
-					int aux=0;
-					if(c==123)
-						aux=1;
-					else if(c==265)
-						aux=2;
-					else if(c==489)
-						aux=3;
-					else
-						aux=4;
-					
-					System.out.println("El estudiante "+lu.Nombre[b]+" "+lu.Apellido[b]+" quedo inscrito en la materia: "+ma.materia[aux]);
-					opcioncorrecta=false;
-					break;
+				menu1.menuprincipal();
+				
+				System.out.println("Seleccione una opcion: ");
+				a = teclado.nextInt();
+				
+		        if (a<1 || a> 4)
+		            System.out.println("Opción incorrecta, ingrese nuevamente... ");
+		        else {
+		        	System.out.println("iGracias por usar este sistema!");
+		        	opcioncorrecta=false;
+		        	break;
+		        }
+			}
+			System.out.println("===============================================================");
+
+			
+			switch (a) {
+			case 1:
+				System.out.println("Inserte el numero del estudiante para inscribir materia mostrado a continuacion:");
+				int i=0;
+				for (String uwu:lu.Nombre) {
+					System.out.println(i+". "+uwu + " "+ lu.Apellido[i]);
+					i=i+1;
 				}
-				else
-					System.out.println("Materia no reconocida, intente poner el ID correcto nuevamente...");
+				int b = teclado.nextInt();
+				
+				
+				System.out.println("Inserte el ID de la materia que desea inscribir para el estudiante: " + lu.Nombre[b]+" "+lu.Apellido[b]);
+				i=0;
+				for (String uwu:ma.materia) {
+					System.out.println("ID:"+ma.ID[i]+" "+uwu);
+					i=i+1;
+				}
+			
+				opcioncorrecta=true;
+				while(opcioncorrecta) {
+					int c = teclado.nextInt();
+					if(c==123 || c==265 || c==348 || c==489) {
+						int aux=0;
+						if(c==123)
+							aux=0;
+						else if(c==265)
+							aux=1;
+						else if(c==348)
+							aux=2;
+						else
+							aux=3;
+						
+						System.out.println("El estudiante "+lu.Nombre[b]+" "+lu.Apellido[b]+" quedo inscrito en la materia: "+ma.materia[aux]);
+						opcioncorrecta=false;
+						datas.DatosDBMemoria1(b,aux);
+						datas.propiedades(b);
+						break;
+					}
+					else
+						System.out.println("Materia no reconocida, intente poner el ID correcto nuevamente...");
+				}
+
+				break;
+			case 2:
+				break;
+			case 3:
+				System.out.println("Cual alumno desea ver su inscripcion: ");
+				i=0;
+				for (String uwu:lu.Nombre) {
+					System.out.println(i+". "+uwu + " "+ lu.Apellido[i]);
+					i=i+1;
+				}
+				int ewe = teclado.nextInt();
+				
+				
+				datas.propiedades(ewe);
+				
+				
+				break;
+			default:
+				System.out.println("Vuelva prontos");
+				break;
 			}
 			
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		default:
-			System.out.println("Vuelva prontos");
-			break;
+			System.out.println("Desea volver a ver el menu principal? 1=si 2=no ");
+			int exit=teclado.nextInt();
+			if (exit==1) {
+				menues=true;
+				opcioncorrecta=true;
+			}
+			else {
+				menues=false;
+				break;
+			}
+		}while(menues);
 		
+		System.out.println("Gracias por usar el sistema...");
 		
-		}
 		teclado.close();
 	}
 
--- app-web/src/test/java/ejercicio/adicional/semana9/DatosDBMemoria.java
+++ app-web/src/test/java/ejercicio/adicional/semana9/DatosDBMemoria.java
@@ -1,5 +1,53 @@
 package ejercicio.adicional.semana9;
 
 public class DatosDBMemoria {
-
+ 
+	Materia iwi = new Materia();
+	Alumno awa = new Alumno();
+	
+	String[] estudiantes = {awa.Nombre[0]+" "+awa.Apellido[0],awa.Nombre[1]+" "+awa.Apellido[1],awa.Nombre[2]+" "+awa.Apellido[2],awa.Nombre[3]+" "+awa.Apellido[3]};
+	String[] materias = {iwi.materia[0],iwi.materia[1],iwi.materia[2],iwi.materia[3]};
+	
+	String[] Datos =  new String[3];
+	String[]estudiante1=new String[4];
+	String[]estudiante2=new String[4];
+	String[]estudiante3=new String[4];
+	String[]estudiante4=new String[4];
+	
+	
+	int i=0;
+	public void DatosDBMemoria1(int idestudiante, int idmateria) {
+		
+	if(idestudiante==0) {
+		estudiante1[0]=estudiantes[0];
+		estudiante1[1]=" "+iwi.materia[idmateria];
+	}
+	if(idestudiante==1) {
+		estudiante2[0]=estudiantes[1];
+		estudiante2[1]=" "+iwi.materia[idmateria];
+	}
+	if(idestudiante==2) {
+		estudiante3[0]=estudiantes[2];
+		estudiante3[1]=" "+iwi.materia[idmateria];
+	}
+	if(idestudiante==3) {
+		estudiante4[0]=estudiantes[3];
+		estudiante4[1]=" "+iwi.materia[idmateria];
+	}
+	}
+	
+	public void propiedades(int est) {
+		if(est==0) {
+			System.out.println("El estudiante: "+estudiante1[0]+" "+estudiante1[1]);
+		}
+		if(est==1) {
+			System.out.println("El estudiante: "+estudiante2[0]+" "+estudiante2[1]);
+		}
+		if(est==2) {
+			System.out.println("El estudiante: "+estudiante3[0]+" "+estudiante3[1]);
+		}
+		if(est==3) {
+			System.out.println("El estudiante: "+estudiante4[0]+" "+estudiante4[1]);
+		}
+	}
 }
